be8e033607e968fd1817fccf0462ddef74d656c8
arunrs-sayone <arun.rs@sayonetech.com>
2023-01-09 16:09:16 +0530

    added prettier

--- src/app.js
+++ src/app.js
@@ -1,39 +1,34 @@
 import React, { Suspense, useState } from "react";
 
-const Home = React.lazy(() => import('pages/home'));
-
-
-
+const Home = React.lazy(() => import("pages/home"));
 
 const App = () => {
-
-  const [state,setstate] = useState('')
+  const [state, setstate] = useState("");
 
   const user = {
     name: "Hayes",
-    id: 123
+    id: 123,
   };
 
-  const Author= {
-    name:'Arun'
-  }
+  const Author = {
+    name: "Arun",
+  };
 
   const messsage = (name) => {
-    return (`hello ${name}`)
-  }
-
+    return `hello ${name}`;
+  };
 
   return (
     <div className="App">
       <h1>Hello World..{user.name}!</h1>
-      <h2 onClick={() => setstate('show')}>Click</h2>
+      <h2 onClick={() => setstate("show")}>Click</h2>
       {state && (
-         <Suspense fallback={<div>Loading...</div>}>
-            <Home/>
+        <Suspense fallback={<div>Loading...</div>}>
+          <Home />
         </Suspense>
       )}
     </div>
   );
-}
+};
 
 export default App;
